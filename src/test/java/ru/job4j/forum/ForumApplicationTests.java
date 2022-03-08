a1fa75a2f4251519f6b0dff0b367ba14c774c829
AndreyPolegaev <79646307753@yandex.ru>
2022-03-08 23:26:48 +0300

    Spring test

--- src/test/java/ru/job4j/forum/ForumApplicationTests.java
+++ src/test/java/ru/job4j/forum/ForumApplicationTests.java
@@ -1,13 +1,93 @@
 package ru.job4j.forum;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
-class ForumApplicationTests {
+@SpringBootTest(classes = Main.class)
+@AutoConfigureMockMvc
+class IndexControlTest {
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
-    void contextLoads() {
+    @WithMockUser
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/index"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
     }
 
+    @Test
+    @WithMockUser
+    public void shouldReturnDefaultMessage2() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+
+    @Test
+    @WithMockUser
+    public void shouldReturnDefaultMessage3() throws Exception {
+        this.mockMvc.perform(get("/users"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("users"));
+    }
+
+    @Test
+    @WithMockUser
+    public void shouldReturnDefaultMessage4() throws Exception {
+        this.mockMvc.perform(get("/update?id=1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("edit"));
+    }
+
+    @Test
+    @WithMockUser
+    public void shouldReturnDefaultMessage5() throws Exception {
+        this.mockMvc.perform(post("/updateItem"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/index"));
+    }
+
+    @Test
+    @WithMockUser
+    public void shouldReturnDefaultMessage6() throws Exception {
+        this.mockMvc.perform(get("/create"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("create"));
+    }
+
+    @Test
+    @WithMockUser
+    public void shouldReturnDefaultMessage7() throws Exception {
+        this.mockMvc.perform(get("/login"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"));
+    }
+
+    @Test
+    @WithMockUser
+    public void shouldReturnDefaultMessage8() throws Exception {
+        this.mockMvc.perform(get("/reg"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("reg"));
+    }
 }
---
+++ src/test/java/ru/job4j/forum/control/IndexControlTest.java
@@ -0,0 +1,7 @@
+package ru.job4j.forum.control;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IndexControlTest {
+
+}
